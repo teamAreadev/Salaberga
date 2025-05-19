81a37920e2cf976dd1b0bc397e53ffc781747169
Uchôa <cavalcanterogeer@gmail.com>
2025-05-19 17:06:26 -0300

    Mudança no login e no redirecionamento do subystemas

--- portalsalaberga/app/subsystems/estagio/views/dashboard.php
+++ portalsalaberga/app/subsystems/estagio/views/dashboard.php
@@ -1,10 +1,12 @@
 <?php
+
 require_once('../models/select_model.php');
 require_once('../models/sessions.php');
 $select_model = new select_model();
 $session = new sessions;
 $session->tempo_session();
 $session->autenticar_session();
+
 
 if (isset($_POST['layout'])) {
     $session->quebra_session();
