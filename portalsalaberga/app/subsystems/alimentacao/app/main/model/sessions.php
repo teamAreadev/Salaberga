e2fbab76b7bb5790dc5bffeebeab4d3091d2b39b
DevRoger7 <cavalcanterogeer@gmail.com>
2025-06-18 11:24:22 -0300

    .

---
+++ portalsalaberga/app/subsystems/alimentacao/app/main/model/sessions.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+
+class sessions
+{
+
+    function autenticar_session()
+    {
+        // DEBUG: Remova depois de testar!
+        // error_log('SESSION: ' . print_r($_SESSION, true));
+
+        if (!isset($_SESSION['login']) && !isset($_SESSION['estagio_adm'])) {
+            unset($_SESSION['login']);
+            unset($_SESSION['status']);
+            header('location:../../../main/views/autenticacao/login.php');
+            exit();
+        } 
+        
+        // $logado = $_SESSION['Login']; // Só use se for necessário
+    }
+    function tempo_session($tempo = 600)
+    {
+        if (isset($_SESSION['ultimo_acesso'])) {
+            if (time() - $_SESSION['ultimo_acesso'] > $tempo) {
+                session_unset();
+                session_destroy();
+                header('location:../../../main/views/autenticacao/login.php');
+                exit();
+            }
+        }
+        $_SESSION['ultimo_acesso'] = time();
+    }
+    function quebra_session()
+    {
+        session_unset();
+        session_destroy();
+        header('location:../../../main/views/autenticacao/login.php');
+        exit();
+    }
+}
