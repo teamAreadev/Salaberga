d8a815d73162c6b0c4f94c7c63c1bc0fdd320072
Roger <uchoadev16@gmail.com>
2025-06-24 09:06:35 -0300

    criação dos controllers das tarefas

---
+++ portalsalaberga/app/subsystems/sesmated/controllers/controller_vestimentas_sustentaveis.php
@@ -0,0 +1,30 @@
+<?php
+require_once('../models/main.model.php');
+
+if (
+    isset($_POST['criterio']) && !empty($_POST['criterio']) &&
+    isset($_POST['pontuacao']) && !empty($_POST['pontuacao']) &&
+    isset($_POST['curso']) && !empty($_POST['curso'])
+) {
+    $criterio = $_POST['criterio'];
+    $pontuacao = $_POST['pontuacao'];
+    $curso = $_POST['curso'];
+
+    $main_model = new main_model();
+    $result = $main_model->confirmar_vestimentas($criterio, $pontuacao, $curso);
+
+    switch ($result) {
+        case 1:
+            header('location:../views/vestimentas_sustentaveis.php?confirmado');
+            exit();
+        case 2:
+            header('location:../views/vestimentas_sustentaveis.php?erro');
+            exit();
+        case 3:
+            header('location:../views/vestimentas_sustentaveis.php?ja_confirmado');
+            exit();
+    }
+} else {
+    header('location:../views/vestimentas_sustentaveis.php?empty');
+    exit();
+} 
--- portalsalaberga/app/subsystems/sesmated/models/main.model.php
+++ portalsalaberga/app/subsystems/sesmated/models/main.model.php
@@ -70,30 +70,292 @@ class main_model extends connect
         }
     }
     //mascote
-    public function confirmar_mascote($criterios, $pontuacao, $id_curso)
+    public function confirmar_mascote($curso_id, $nota_animacao, $nota_vestimenta, $nota_identidade)
+    {
+        // Verifica se já existe registro para o curso
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_03_mascote WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $curso_id);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO tarefa_03_mascote (curso_id, animacao, vestimenta, identidade_curso) VALUES (:curso_id, :animacao, :vestimenta, :identidade)");
+            $stmt_adcionar->bindValue(':curso_id', $curso_id);
+            $stmt_adcionar->bindValue(':animacao', $nota_animacao);
+            $stmt_adcionar->bindValue(':vestimenta', $nota_vestimenta);
+            $stmt_adcionar->bindValue(':identidade', $nota_identidade);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //logo
+    public function confirmar_logo($criterios, $pontuacao, $id_curso)
     {
         // Verifica se já existe registro para a turma
-        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_02_grito_guerra WHERE curso_id = :curso_id");
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_04_logo WHERE curso_id = :curso_id");
         $stmt_check->bindValue(':curso_id', $id_curso);
         $stmt_check->execute();
         $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
 
         if (empty($result)) {
-
-            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_02_grito_guerra`(`curso_id`, `cumprida`, `pontuacao`) VALUES (:curso_id, :cumprida, :pontuacao)");
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_04_logo`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
             $stmt_adcionar->bindValue(':curso_id', $id_curso);
-            $stmt_adcionar->bindValue(':cumprida', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
             $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
 
             if ($stmt_adcionar->execute()) {
-
                 return 1;
             } else {
-
                 return 2;
             }
         } else {
+            return 3;
+        }
+    }
 
+    //esquete
+    public function confirmar_esquete($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_05_esquete WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_05_esquete`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //cordel
+    public function confirmar_cordel($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_06_cordel WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_06_cordel`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //parodia
+    public function confirmar_parodia($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_07_parodia WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_07_parodia`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //vestimentas sustentaveis
+    public function confirmar_vestimentas($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_08_vestimentas WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_08_vestimentas`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //workshops
+    public function confirmar_workshops($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_09_workshops WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_09_workshops`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //sala temática
+    public function confirmar_sala_tematica($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_10_sala_tematica WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_10_sala_tematica`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //palestras
+    public function confirmar_palestras($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_11_palestras WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_11_palestras`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //painel
+    public function confirmar_painel($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_12_painel WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_12_painel`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //empreendedorismo
+    public function confirmar_empreendedorismo($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_13_empreendedorismo WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_13_empreendedorismo`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
+    }
+
+    //inovacao
+    public function confirmar_inovacao($criterios, $pontuacao, $id_curso)
+    {
+        $stmt_check = $this->connect->prepare("SELECT * FROM tarefa_14_inovacao WHERE curso_id = :curso_id");
+        $stmt_check->bindValue(':curso_id', $id_curso);
+        $stmt_check->execute();
+        $result = $stmt_check->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($result)) {
+            $stmt_adcionar = $this->connect->prepare("INSERT INTO `tarefa_14_inovacao`(`curso_id`, `criterios`, `pontuacao`) VALUES (:curso_id, :criterios, :pontuacao)");
+            $stmt_adcionar->bindValue(':curso_id', $id_curso);
+            $stmt_adcionar->bindValue(':criterios', json_encode($criterios));
+            $stmt_adcionar->bindValue(':pontuacao', $pontuacao);
+
+            if ($stmt_adcionar->execute()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
             return 3;
         }
     }
