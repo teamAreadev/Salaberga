6cfbe0be4ed9bee6338ebc8f6719694a5e66a022
DevRoger7 <cavalcanterogeer@gmail.com>
2025-06-25 10:04:52 -0300

    Create pageRelatorios.php

---
+++ portalsalaberga/app/subsystems/sesmated/views/pageRelatorios.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<h1>Relatório de Avaliadores</h1>
+<form action="relatorios/avaliadores/avaliadores.php" method="get" target="_blank">
+    <button type="submit">Gerar Relatório</button>
+</form>
+
+<h1>Relatório de Frequência de Eventos</h1>
+<form action="relatorios/rifas/frequenciaEventos.php" method="get" target="_blank">
+    <button type="submit">Gerar Relatório</button>
+</form>
+
+<h1>Relatório de Rifas - Resumo por Curso</h1>
+<form action="relatorios/rifas/resumo_curso.php" method="get" target="_blank">
+    <button type="submit">Gerar Relatório</button>
+</form>
+
+<h1>Relatório de Rifas - Resumo Total</h1>
+<form action="relatorios/rifas/resumo_total.php" method="get" target="_blank">
+    <button type="submit">Gerar Relatório</button>
+</form>
+
+<h1>Relatório de Rifas - Resumo por Turma</h1>
+<form action="relatorios/rifas/resumo_turma.php" method="get" target="_blank">
+    <button type="submit">Gerar Relatório</button>
+</form>
