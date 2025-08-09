41b96c8417f6357c9630f682a2d212aabb48d8d6
Matheus Felix <matheus.dev91@gmail.com>
2025-08-09 20:58:01 -0300

    teste1

--- portalsalaberga/app/main/index.php
+++ portalsalaberga/app/main/index.php
@@ -52,7 +52,7 @@
 
     <!-- Fontes -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnec" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800&family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <script src="https://cdn.tailwindcss.com"></script>
