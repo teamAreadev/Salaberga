edab0aa599740b490047a0d1772799e564765b0c
Uchôa <164269740+Uchoadev16@users.noreply.github.com>
2025-08-12 23:39:07 -0300

    Update connection.php

--- portalsalaberga/app/subsystems/ControledeEstoque/app/main/config/connection.php
+++ portalsalaberga/app/subsystems/ControledeEstoque/app/main/config/connection.php
@@ -1,46 +1,43 @@
 <?php
-class Connection {
+class Connection
+{
     protected $pdo;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->connect_database();
     }
-    
-    public function connect_database() {
-        // Configurações de conexão local
-        $local_dsn = "mysql:host=localhost;dbname=u750204740_estoque;charset=utf8mb4";
-        $local_user = "root";
-        $local_pass = "";
-        
-        // Configurações de conexão da hospedagem
-        $host_dsn = "mysql:host=u750204740_estoque;dbname=u750204740_estoque;charset=utf8mb4";
-        $host_user = "u750204740_estoque";
-        $host_pass = "paoComOvo123!@##";
-        
-        // Tenta conexão local primeiro
+
+    public function connect_database()
+    {
+
         try {
+            $local_dsn = "mysql:host=localhost;dbname=u750204740_estoque;charset=utf8mb4";
+            $local_user = "root";
+            $local_pass = "";
             $this->pdo = new PDO($local_dsn, $local_user, $local_pass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // echo "Conexão local estabelecida com sucesso!";
             return true;
         } catch (PDOException $e) {
             // echo "Erro na conexão local: " . $e->getMessage();
-            
-            // Tenta conexão com a hospedagem em caso de falha na local
-            try {
-                $this->pdo = new PDO($host_dsn, $host_user, $host_pass);
-                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // echo "Conexão com hospedagem estabelecida com sucesso!";
-                return true;
-            } catch (PDOException $e) {
-                echo "Erro na conexão com hospedagem: " . $e->getMessage();
-                return false;
-            }
+
+            // Configurações de conexão da hospedagem
+            $host_dsn = "mysql:host=u750204740_estoque;dbname=u750204740_estoque;charset=utf8mb4";
+            $host_user = "u750204740_estoque";
+            $host_pass = "paoComOvo123!@##";
+            $this->pdo = new PDO($host_dsn, $host_user, $host_pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // echo "Conexão com hospedagem estabelecida com sucesso!";
+            return true;
+        } catch (PDOException $e) {
+            echo "Erro na conexão com hospedagem: " . $e->getMessage();
+            return false;
         }
     }
-    
-    public function getPdo() {
+
+    public function getPdo()
+    {
         return $this->pdo;
     }
 }
-?>
